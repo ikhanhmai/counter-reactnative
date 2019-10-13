11ca6a1630ce0d3dd9a383b7a7617e0c38b0fd4d
khanh <ikhanhmai@gmail.com>
2019-10-13 09:57:05 +0700

    finish

--- counter/App.js
+++ counter/App.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
-import Child from './App/components/Child';
-import ButtonComp from './App/components/Button';
+import {View} from 'react-native';
+import Main from './App/components/Main';
 import {Provider} from 'react-redux';
 import store from './App/store';
-class Main extends Component {
+export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
@@ -14,43 +13,9 @@ class Main extends Component {
             width: '100%',
             justifyContent: 'center',
           }}>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Child />
-          </View>
-          <View style={{flex: 1}}>
-            <ButtonComp
-              title="Increase"
-              textColor="#fff"
-              bgColor="#397af8"
-              onPress={() => {}}
-            />
-            <ButtonComp title="Decrease" bgColor="orange" onPress={() => {}} />
-          </View>
+          <Main />
         </View>
       </Provider>
     );
   }
 }
-
-export default Main;
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'red',
-  },
-  btnStyle: {
-    width: 100,
-    height: 40,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: 'red',
-    backgroundColor: '#15c',
-  },
-});
--- counter/App/components/Child.js
+++ counter/App/components/Child.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-
+import {connect} from 'react-redux';
 class Child extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <View>
-        <Text style={styles.text}>0</Text>
+        <Text style={styles.text}>{this.props.counter}</Text>
       </View>
     );
   }
 }
-
-export default Child;
+mapStateToPros = state => ({
+  counter: state.counter,
+});
+export default connect(
+  mapStateToPros,
+  null,
+)(Child);
 
 const styles = StyleSheet.create({
   text: {
